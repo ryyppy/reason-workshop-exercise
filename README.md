25dda5c6b55ee4285f4af73cde2a43d4a98d7c13
Nik Graf <nik@nikgraf.com>
2018-04-24 12:06:55 +0300

    move reason setup to step 2

--- README.md
+++ README.md
@@ -4,40 +4,6 @@ This repo is built on the default gatsby boilerplate and contains a
 challenge the workshop attendees are supposed to solve. More details on
 the workshop can be found [here](https://github.com/ryyppy/reason-workshop).
 
-
-We will gradually introduce BuckleScript in this setting. To do so run
-
-```
-yarn add --dev bs-platform
-# or
-npm install --save-dev bs-platform
-```
-
-To make things easier, here is a simple `bsconfig.json`
-boilerplate (place this code in `./bsconfig.json`):
-
-```
-{
-  "name": "reason-workshop-gatsby",
-  "version": "0.1.0",
-  "reason": { "react-jsx": 2 },
-  "sources": {
-    "dir": "src",
-    "subdirs": true
-  },
-  "package-specs": {
-    "module": "commonjs",
-    "in-source": true
-  },
-  "suffix": ".js",
-  "bs-dependencies": [
-  ],
-  "bsc-flags": ["-bs-no-version-header", "-bs-super-errors"]
-}
-```
-
-Add a script to compile the Reason files: e.g. `"bs:watch": "bsb -make-world -w"`
-
 ## Install
 
 ```
@@ -53,3 +19,5 @@ npm run develop
 # or
 yarn run develop
 ```
+
+Visit `http://localhost:8000` to see further instructions.
--- src/pages/step2.js
+++ src/pages/step2.js
@@ -3,17 +3,43 @@ import Link from "gatsby-link";
 
 import ColorSquare from "../components/colorSquare";
 
+const usageExample = `<ColorSquare color="green" />
+<ColorSquare color="red" size="50" />
+<ColorSquare size={100} />`;
+
+const helloWorldExample = `let component = ReasonReact.statelessComponent("HelloWorld");
+
+let make = _children => {
+  ...component,
+  render: _self => <div> (ReasonReact.stringToElement("HelloWorld")) </div>,
+};
+
+let default = ReasonReact.wrapReasonForJs(~component, _jsProps => make([||]));`;
+
+const bsConfig = `{
+  "name": "reason-workshop-gatsby",
+  "version": "0.1.0",
+  "reason": {
+    "react-jsx": 2
+  },
+  "sources": {
+    "dir": "src",
+    "subdirs": true
+  },
+  "package-specs": {
+    "module": "commonjs",
+    "in-source": true
+  },
+  "suffix": ".js",
+  "bs-dependencies": ["reason-react"],
+  "bsc-flags": ["-bs-no-version-header", "-bs-super-errors"]
+}`;
+
 const Step2 = () => (
   <div>
     <h1>Let{"'"}s use it in our Mars Rocket!</h1>
     <div>
-      <pre>
-        {`
-        <ColorSquare color="green" />
-        <ColorSquare color="red" size="50" />
-        <ColorSquare size={100} />
-        `}
-      </pre>
+      <pre>{usageExample}</pre>
     </div>
     <div>
       <ColorSquare color="green" />
@@ -31,6 +57,29 @@ const Step2 = () => (
     <p>
       Let{"'"}s make sure this won{"'"}t happen in the future. Let{"'"}s go back
       to step one and re-write it to Reason.
+    </p>
+
+    <ul>
+      <li>Run `yarn add --dev bs-platform` and `yarn add reason-react`</li>
+      <li>
+        Create a `bsconfig.json` in the project root directory:
+        <pre>{bsConfig}</pre>
+      </li>
+      <li>
+        Add a script to compile the Reason files: e.g. `bs:watch": "bsb
+        -make-world -w`
+      </li>
+      <li>
+        As a starting point you can add the file `exercise.re` and use this
+        code:
+        <pre>{helloWorldExample}</pre>
+      </li>
+    </ul>
+
+    <p>
+      This automatically will compile to `exercise.js` and due how Gatsby works
+      you can see the result in{" "}
+      <Link to="/exercise">http://localhost:8000/exercise</Link>
     </p>
 
     <div>
