ac80e7c11cedbadab48cee6efb2a9644c19e8545
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-09-16 16:33:34 +0200

    First ColorSquare impl in JS

--- src/components/colorSquare.js
+++ src/components/colorSquare.js
@@ -1,7 +1,24 @@
 import React from "react";
 
-const ColorSquare = props => {
-  return <div>Not implemented</div>;
+const ColorSquare = ({color, size}) => {
+  let backgroundColor;
+  switch(color) {
+    case "green":
+    case "yellow":
+    case "red":
+    case "papayawhip":
+      backgroundColor = color;
+      break;
+    default:
+      backgroundColor = "papayawhip";
+  };
+  const style = {
+    backgroundColor,
+    height: size,
+    width: size,
+  };
+  
+  return <div style={style}></div>;
 };
 
 export default ColorSquare;
